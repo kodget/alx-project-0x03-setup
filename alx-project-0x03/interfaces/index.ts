d32b2eb69c979f09a7530bf9eb07d9812403e141
Muhammad <muminmuhsina1@gmail.com>
2025-09-26 08:27:28 -0700

    Refractor: interface structure

--- alx-project-0x03/interfaces/index.ts
+++ alx-project-0x03/interfaces/index.ts
@@ -1,6 +1,14 @@
+export interface LayoutProps {
+  children: ReactNode;
+}
+
 export interface ButtonProps {
   buttonLabel: string;
   buttonSize?: string;
   buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
   action?: () => void;
+}
+
+export interface PageRouteProps {
+  pageRoute: string;
 }
